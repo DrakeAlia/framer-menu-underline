bd3d2fb8737286014dbdbe764bbf10a45a266121
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-09-02 20:27:57 -0700

    added comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { useState } from "react";
-import { motion, AnimateSharedLayout } from "framer-motion";
+// import motion and AnimateSharedLayout*(v5 removes AnimateSharedLayout)
+import { motion } from "framer-motion";
 import { screens } from "./data";
 
 export default function App() {
   const [selected, setSelected] = useState(0);
 
   return (
-    <AnimateSharedLayout transition={{ duration: 0.5 }}>
+    // add transition prop with object of duration
+    <motion.ul layout transition={{ duration: 0.5 }}>
       <ol>
+        {/* add motion to li */}
         {screens.map(({ title, color }, i) => (
           <motion.li
             key={i}
             className={`title ${i === selected && "selected"}`}
             style={{ color: i === selected ? color : "#333" }}
             onClick={() => setSelected(i)}
+            // add animate prop
             animate
           >
             {i === selected && (
               <motion.div
                 className="underline"
+                // add layoutId prop with the string of underline
                 layoutId="underline"
                 style={{ backgroundColor: color }}
               />
@@ -28,6 +33,6 @@ export default function App() {
           </motion.li>
         ))}
       </ol>
-    </AnimateSharedLayout>
+    </motion.ul>
   );
 }
